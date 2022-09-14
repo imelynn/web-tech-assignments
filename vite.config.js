77a060728fc4be9c6b93dbfb0882857a15b6eb4e
E. Lynn <imelynnoffical@gmail.com>
2022-09-14 18:36:45 -0400

    Vite Config Update

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         'Breakout Game/index': resolve(__dirname, 'Breakout Game/index.html'),
         'comment-app/index': resolve(__dirname, 'comment-app/index.html'),
         'form-input/index': resolve(__dirname, 'form-input/index.html'),
-        'omdb_final_working2': resolve(__dirname, 'omdb_final_working2/index.html'),
+        'final_omdb_check': resolve(__dirname, 'final_omdb_check/index.html'),
         'picsum-lookup': resolve(__dirname, 'picsum-lookup/index.html'),
         'tech-document': resolve(__dirname, 'tech-document/index.html'),
       },

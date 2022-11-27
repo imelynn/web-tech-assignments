5dc4db7a5a3e2b12ca3de8f9179c2154275fd72c
E. Lynn <imelynnoffical@gmail.com>
2022-11-27 09:27:31 -0500

    ResponsiveD Updated

--- wpa/main.js
+++ wpa/main.js
@@ -1,13 +1,20 @@
 //install service working immediately
-/*if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js');
-}*/
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('/service-worker.js', {scope: './'})
+    .then(function(registration){
+        console.log("Service Worker Registered, registration");
+    })
+    .catch(function(err) {
+        console.log("Service Worker Failed to Register", err);
+    })
+}
 
 // Register the service worker
 
-window.addEventListener('load', () => {
+/*window.addEventListener('load', () => {
     navigator.serviceWorker
     .register('/public/servwork.js')
     .then(_ => console.log('Registered service worker'))
     .catch(e => console.log('Error registering: ',e));
-  });
+  });*/
